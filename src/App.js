c36a574675eb9f24eeff4b4a521a2feee64611dd
Max <maxduma16@gmail.com>
2021-03-04 17:12:37 +0200

    v3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,35 @@ import React from 'react';
 import './App.scss';
 import Layout from './components/layout/Layout';
 
+import UAN from './image/UA.png';
+import CHF from './image/CHF.png';
+import CNY from './image/CNY.png';
+import EUR from './image/EUR.png';
+import GBP from './image/GBP.png';
+import JPY from './image/JPY.png';
+import RUB from './image/RUB.png';
+import USD from './image/USD.png';
+
+
  class App extends React.Component {
+  constructor(props) {
+    super(props); 
+      this.state = {
+        base: 'USD',
+        rate: '',
+        data: '',
+        currency: {
+          'USD': {name: 'Dolar USD', flag: USD, course: '99999' },
+          'UAN': {name: 'Ukrainian Hryvnia', flag: UAN, course: '99999' },
+          'CHF': {name: 'Chinese yuan', flag: CHF, course: '99999' },
+          'EUR': {name: 'Euro', flag: EUR, course: '99999' },
+          'GBP': {name: 'Pound Sterling', flag: GBP, course: '99999' },
+          'JPY': {name: 'Japanese yen', flag: JPY, course: '99999' },
+          'RUB': {name: 'Dolar USD', flag: RUB, course: '99999' },
+          'CNY': {name: 'Swiss franc', flag: CNY, course: '99999' },
+        }
+      };
+  }
 
   render() {
     return (
