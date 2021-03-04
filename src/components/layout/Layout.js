297667851ecc8794bde1f99fb1bc37d504af9d8a
Max <maxduma16@gmail.com>
2021-03-04 16:50:59 +0200

    sidebar

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './layout.scss';
 import AddClass from '../../hoc/AddClass';
 import { Header } from '../header/header';
+import Home from '../../pages/home/home';
+import Sidebar from '../sidebar/sidebar';
 
  const Layout = () => {
 
     return (
-        <Header />
+        <Fragment>
+            <Header />
+            <div className = "content">
+                <div className= "routes">
+                    <Home />
+                </div>
+                <Sidebar />
+            </div>
+        </Fragment>
     )
 }
 
---
+++ src/components/sidebar/sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './sidebar.scss';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebarHead">
+                <h3>All Currencies</h3>
+            </div>
+            <div className="sidebarContent">
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
