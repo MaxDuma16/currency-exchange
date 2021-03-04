7427be4490b4becb0de865ce980690e1fb3e5733
Max <maxduma16@gmail.com>
2021-03-04 15:21:09 +0200

    first commit

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './layout.scss';
+import AddClass from '../../hoc/AddClass';
+
+ const Layout = () => {
+
+    return (
+        <div>
+            <h1>Layout</h1>
+        </div>
+    )
+}
+
+export default AddClass(Layout, 'layout') ;
